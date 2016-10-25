81aa29b00395e1dce53ce06e0684864f8c7db386
Ivor Staats <ivor.staats@student.aventus.nl>
2016-10-25 13:26:02 +0200

    xD

--- index.php
+++ index.php
@@ -32,36 +32,65 @@ if(isset($_POST['resetGame']))
     $game->resetGame();
 }
 
-
-echo "<pre>";
-var_dump($storage->get('player'));
-var_dump($storage->get('dealer'));
-echo $game->calcCards('player');
-echo "<br>";
-echo $game->calcCards('dealer');
-
 ?>
-<div class="playercards">
-    <p>Player Cards</p>
-    <?php
-    foreach($storage->get('player') as &$key)
-    {
-        echo "<img src='svgcardimages/{$key[0]}.svg' alt='{$key[0]}' />";
-    }
-    ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+    <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+<body>
+<div id="logo" class="container">
+    <h1><a href="#">Black<span>Jack</span></a></h1>
 </div>
-<div class="dealercards">
-    <p>Dealer Cards</p>
-    <?php
-    foreach($storage->get('dealer') as &$key)
-    {
-        echo "<img src='svgcardimages/{$key[0]}.svg' alt='{$key[0]}' />";
-    }
-    ?>
+<div id="menu" class="container">
+    <ul>
+        <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+        <li><a href="#" accesskey="1" title="">Services</a></li>
+        <li><a href="#" accesskey="2" title="">Our Clients</a></li>
+        <li><a href="#" accesskey="3" title="">About Us</a></li>
+        <li><a href="#" accesskey="4" title="">Careers</a></li>
+        <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+    </ul>
 </div>
-<form action="index.php" method="post">
-    <input type="submit" name="giveCard" value="Hit">
-    <input type="submit" name="keepCard" value="Stand">
-    <input type="submit" name="startGame" value="start game">
-    <input type="submit" name="resetGame" value="reset game">
-</form>
+
+<div id="banner" class="container">
+    <div class="dealercards" style="">
+        <p>Dealer cards</p>
+        <?php
+        foreach($storage->get('dealer') as &$key)
+        {
+            echo "<img src='svgcardimages/{$key[0]}.svg' alt='{$key[0]}' />";
+        }
+        ?>
+    </div>
+    <div class="playercards">
+        <p>Player cards</p>
+        <?php
+        foreach($storage->get('player') as &$key)
+        {
+            echo "<img src='svgcardimages/{$key[0]}.svg' alt='{$key[0]}' />";
+        }
+        ?>
+    </div>
+
+    <form action="index.php" method="post">
+        <input type="submit" name="giveCard" value="Hit">
+        <input type="submit" name="keepCard" value="Stand">
+        <input type="submit" name="startGame" value="start game">
+        <input type="submit" name="resetGame" value="reset game">
+    </form>
+</div>
+<div id="three-column" class="container">
+
+</div>
+</body>
+</html>
+
